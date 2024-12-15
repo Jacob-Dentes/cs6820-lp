8eca4f764659bea361f900a1f18c5c41418bcd99
Jacob Dentes <jmd477@cornell.edu>
2024-12-15 17:19:20 -0500

    Created an initial analysis graphing script

---
+++ analysis.py
@@ -0,0 +1,73 @@
+import timeit
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+import test_lp
+from simplex import *
+
+# seed the generator for consistent results
+SEED = 0
+random.seed(SEED)
+np.random.seed(SEED)
+
+# number of times to run each algorithm
+TRIALS = 10
+
+simplex_closure = lambda x: lambda a, b, c: simplex(a, b, c, x)
+bland_simplex = simplex_closure(blands_rule)
+zadeh_simplex = simplex_closure(zadehs_rule)
+cunningham_simplex = simplex_closure(cunninghams_rule)
+
+# time functions in list fs on TRIALS knapsack instances of size n
+def test_knapsack(fs, n):    
+    instances = [test_lp.formulate_gaussian_knapsack(n) for _ in range(TRIALS)]
+    instances = [(x.get_A(), x.get_b(), x.get_c()) for _, x in instances]
+
+    times = []
+    for f in fs:
+        func = lambda: [f(A, b, c) for A, b, c in instances]
+        times.append(timeit.timeit(func, number=1))
+    
+    return tuple(times)
+
+fs = [bland_simplex, zadeh_simplex, cunningham_simplex]
+x = list(range(5, 10))
+results = [test_knapsack(fs, n) for n in x]
+
+b, z, c = list(map(list, zip(*results)))
+
+plt.plot(x, b, label="Bland's rule")
+plt.plot(x, z, label="Zadeh's rule")
+plt.plot(x, c, label="Cunningham's rule")
+
+plt.legend()
+plt.show()
+
+
+# time functions in list fs on TRIALS knapsack instances of size n
+def test_kleeminty(fs, n):    
+    instances = [test_lp.formulate_kleeminty_cube(n) for _ in range(TRIALS)]
+    instances = [(x.get_A(), x.get_b(), x.get_c()) for _, x in instances]
+
+    times = []
+    for f in fs:
+        func = lambda: [f(A, b, c) for A, b, c in instances]
+        times.append(timeit.timeit(func, number=1))
+    
+    return tuple(times)
+
+fs = [bland_simplex, zadeh_simplex, cunningham_simplex]
+x = list(range(5, 10))
+results = [test_kleeminty(fs, n) for n in x]
+
+b, z, c = list(map(list, zip(*results)))
+
+plt.plot(x, b, label="Bland's rule")
+plt.plot(x, z, label="Zadeh's rule")
+plt.plot(x, c, label="Cunningham's rule")
+
+plt.legend()
+plt.show()
+
